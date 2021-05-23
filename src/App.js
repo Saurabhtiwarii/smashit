5ccc2b60eef5e51df18a8579c4e5c87c03d669f9
saurabh <sourabhkt19@gmail.com>
2021-05-23 12:01:36 +0530

    countdown problem fixed and background sound replay fixed

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { useState } from 'react';
 import avatar1 from './images/avatar1.png';
 import avatar2 from './images/avatar2.png';
 import avatar3 from './images/avatar3.png';
+import music from './images/music.mp3';
+import sound from './images/sound.wav';
 
 
 function App() {
+
   let [count,setCount] = useState(0);
   let [score,setScore] = useState(0);
   let [avatar, setAvatar] = useState(avatar1);
@@ -16,7 +19,7 @@ function App() {
   let lastHole = null;
   let countdown;
 
-
+  
   const avatars =document.querySelectorAll('.avatar');
 //* pick the random hole
 function pickupRandomHole(){
@@ -24,14 +27,14 @@ function pickupRandomHole(){
   const randomHole = Math.floor(Math.random()*holes.length);
   const hole = holes[randomHole];
   if (hole === lastHole){
-      return pickupRandomHole(holes);
+      return pickupRandomHole();
   }
   lastHole = hole;
   return hole;
 }
 //* popout the avatar from hole
 function popoutAvatar(){
-const time = Math.random() * (700 - 400) + 400;
+const time = Math.random() * (800 - 400) + 400;
 const hole = pickupRandomHole();
 hole.classList.add('up');
 setTimeout(function(){
@@ -43,6 +46,7 @@ setTimeout(function(){
 
  // play the game
   function playgame(){
+    const countdownBoard = document.querySelector('.countdown-board');
     countdown = timeLimit/1000;
     setCount(countdown);
     timeUp = false; 
@@ -58,38 +62,62 @@ setTimeout(function(){
         countdown = 0;
         clearInterval(startCountDown);
         setCount('Time Up');
+        const gameoverScreen = document.querySelector('.gameover-screen');
+        gameoverScreen.style.display = 'flex';
+        
+    }
+
+    if(countdown == 30){
+      countdownBoard.style.color = '#f38e4b'
+    }
+
+    if(countdown == 15){
+      countdownBoard.style.color = '#f34b4b'
     }
 }, 1000);
 }
 
 
   function startGame(){
+      setScore(0);
+     const countdownBoard = document.querySelector('.countdown-board');
+      countdownBoard.classList.remove('countdown-board-active');
+      countdownBoard.style.transition = 'none';
+    const background_music = document.querySelector('.background_music');
+    background_music.pause();
     const selection_screen = document.querySelector('.players');
     selection_screen.style.display = 'none';
     const readyWindow = document.querySelector('.ready-window');
     readyWindow.style.display = 'flex';
     let count = 0;
-
+    
       const readyCountdown =  setInterval(()=>{
           const readyWindowText = document.querySelector('.ready-window span');
           count = count+1;
-          if(count === 2){
+          if(count === 1){
             readyWindowText.innerText = '3';
           }
-          else if(count === 3){
+          else if(count === 2){
             readyWindowText.innerText = '2';
           }
-          else if(count === 4){
+          else if(count === 3){
             readyWindowText.innerText = '1';
           } 
-          else if(count === 5){
+          else if(count === 4){
             readyWindow.style.display = 'none';
             clearInterval(readyCountdown);
             playgame();
+            countdownBoard.style.transition = 'transform 0.7s';
+            countdownBoard.classList.add('countdown-board-active');
+
           }
+         
     },1000);
   }
   function hit(){
+    const hit_sound = document.querySelector('.hit_sound');
+    hit_sound.currentTime = 0;
+    hit_sound.play();
     score = score+1;
     setScore(score)
   }
@@ -100,10 +128,16 @@ setTimeout(function(){
   
 
 function selectionScreen(){
+  const gameoverScreen = document.querySelector('.gameover-screen');
+  gameoverScreen.style.display = 'none';
+  const background_music = document.querySelector('.background_music');
   const introWindow = document.querySelector('.intro-window');
   const selection_screen = document.querySelector('.players');
   introWindow.style.transform = 'translateY(100%)';
+  introWindow.style.display = 'none';
   selection_screen.style.display = 'flex';
+  background_music.currentTime = 0;
+  background_music.play();
 
 }
 
@@ -137,7 +171,16 @@ function avatarSelection(n){
  
   return (
     <div className="App">
+<audio loop className="background_music">
+  <source src={ music } type="audio/ogg"/>
+Your browser does not support the audio element.
+</audio>
 
+<video className="hit_sound">
+  <source src={ sound } type="video/wav"/>
+  <source src={ sound } type="video/ogg"/>
+  Your browser does not support the video tag.
+</video>
            
 
  <div className="players">
@@ -162,19 +205,20 @@ function avatarSelection(n){
                       
                   </ul>
                   <button className="continue-btn" onClick={startGame}>Continue</button>
+</div>
+
+            <div className="intro-window">
+              <h1>Smash It</h1>
+              <button className="start-btn" onClick={selectionScreen}>START</button>
             </div>
   
       <div className="game">  
            
-      <div className="intro-window">
-              <h1>Smash It</h1>
-              <button className="start-btn" onClick={selectionScreen}>START</button>
-            </div>
-
+    
             <div className="ready-window">
               <span>Ready ?</span>
             </div>
-
+            <div className="countdown-board">countdown: <span className="countdown">{count}</span></div>
             <div className="game__ground">
               <div className="hole hole1">
                   <div className="avatar sm">
@@ -212,9 +256,14 @@ function avatarSelection(n){
                   </div>
               </div>
         </div>
-
+            <div className="gameover-screen">
+                <div className="gameover-screen__message">Game Over</div>
+                <div className="gameover-screen__score">Score: {score}</div>
+                <button className="back-btn" onClick={selectionScreen}>Play Again</button>
+          </div>
       </div>
-          <Footer count = {count} score = {score}/>
+          <Footer  score = {score}/>
+         
     </div>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer(props) {
     return (
         <div className="game__footer">   
-        <div className="countdown-board">countdown: <span className="countdown">{props.count}</span></div>
+        
         <div className="score-board">Score: <span className="score">{props.score}</span></div>
       </div>
     )
